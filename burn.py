c24ebac73314f13772829e08bebf16b1958b142b
hardanimal <hardanimal@hotmail.com>
2024-03-18 09:07:49 +0800

    Consume multi core CPU.

--- burn.py
+++ burn.py
@@ -1,3 +1,19 @@
-while(True):
-    i = 1 + 1
+# encoding: utf-8
 
+__author__ = 'Danzel.Li'
+
+import os
+import _thread
+from multiprocessing import Process
+
+def foo():
+    while(True):
+        i = 1 + 1
+
+if __name__ == "__main__":
+    cpu = os.cpu_count()
+    for i in range(cpu):
+        p = Process(target = foo, args = ())
+        p.start()
+
+    input()
